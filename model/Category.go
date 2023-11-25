6784179263752f0576a496c46916f59ac371e029
Hufeng <2506434902@qq.com>
2023-11-25 21:18:25 +0800

    push 11-25

--- model/Category.go
+++ model/Category.go
@@ -1,1 +1,13 @@
 package model
+
+import "reflect"
+
+type Category struct {
+	Universal
+	Name     string    `gorm:"type:varchar(20);not null;comment:分类名称" json:"name"`
+	Articles []Article `gorm:"foreignKey:CategoryId"` // 重写外键？
+}
+
+func (c *Category) IsEmpty() bool {
+	return reflect.DeepEqual(c, &Category{})
+}
--- service/category.go
+++ service/category.go
@@ -2,8 +2,11 @@ package service
 
 import (
 	"gin-blog-hufeng/dao"
+	"gin-blog-hufeng/model"
 	"gin-blog-hufeng/model/req"
 	"gin-blog-hufeng/model/resp"
+	"gin-blog-hufeng/utils"
+	"gin-blog-hufeng/utils/r"
 )
 
 type Category struct{}
@@ -20,5 +23,17 @@ func (*Category) GetList(req req.PageQuery) resp.PageResult[[]resp.CategoryVo] {
 
 func (*Category) SaveOrUpdate(req req.AddOrEditCategory) int {
 	// 查询目标分类是否存在
-	existByName := dao.GetOne(model.)
+	existByName := dao.GetOne(model.Category{}, "name = ?", req.Name)
+	// 同名存在 && 存在的ID不等于当前要更新的ID -> 重复
+	if !existByName.IsEmpty() && existByName.ID != req.ID {
+		return r.ERROR_CATE_NAME_USED
+	}
+	category := utils.CopyProperties[model.Category](req)
+
+	if req.ID != 0 {
+		dao.Update(&category, "name")
+	} else {
+		dao.Create(&category)
+	}
+	return r.OK
 }
